e191a471a5fca5a74a84bdd83404082dee15534c
ichigar <ichigar@gobiernodecanarias.org>
2022-11-26 14:11:03 +0000

    sáb 26 nov 2022 14:11:02 WET

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat /etc/apt/sources.list /etc/apt/sources.list.d/*.list | grep jammy | grep --invert-match "#" > sources.list
-apt list > apt_packages.text
+apt list > apt_packages.txt
 snap list | cut -f1 -d" " > snaps.txt 
 cp ~/.bashrc .
 cp ~/.gitconfig .
